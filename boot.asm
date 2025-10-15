0fcccf2f6c23cc7035d0a6b804922103e801333f
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2025-10-15 14:39:46 +0100

    Boot.ASM
    Now 32 Bit!

--- boot.asm
+++ boot.asm
@@ -25,23 +25,68 @@ VolumeLabel		db "MyBOOT     "; Volume Label: any 11 chars
 FileSystem		db "FAT12   "	; File system type: don't change!
 
 INIT:
-   xor ax, ax  ;make it zero
+   xor ax, ax
    mov ds, ax
    cld
-
+   mov ah, 00h
+   mov al, 03h
+   int 10h
    mov si, msg
    call bios_print
+   in al, 0x92
+   or al, 2
+   out 0x92, al
+   lgdt [gdt_descriptor]
+
+   cli
+   nop
+   mov eax, cr0
+   or eax, 1
+   mov cr0, eax
+   jmp CODE_SEG:halt
 
 
 halt:
-   jmp halt
+   jmp $
 
 msg   db 'Bootloader Works, Very Good!', 13, 10, 0
+
+; GDT, Taken From UntitledOS
+gdt_start:
+    ; Null descriptor
+    dd 0x0
+    dd 0x0
+    
+    ; Code segment descriptor
+    dw 0xffff    ; Limit (bits 0-15)
+    dw 0x0       ; Base (bits 0-15)
+    db 0x0       ; Base (bits 16-23)
+    db 10011010b ; Flags
+    db 11001111b ; Flags + Limit (bits 16-19)
+    db 0x0       ; Base (bits 24-31)
+    
+    ; Data segment descriptor
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+    
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1  ; GDT size
+    dd gdt_start                 ; GDT address
+
+; Constants
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
 
 bios_print:
    lodsb
    or al, al  ;zero=end of str
-   jz done    ;get out
+   jz done    ;GET OUT
    mov ah, 0x0E
    mov bh, 0
    int 0x10
